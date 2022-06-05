3758ca854700bccfe76b8927a3892f89efcabe78
SongYii <49025999+SongYii@users.noreply.github.com>
2022-06-05 22:39:59 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -6,17 +6,5 @@ int main()
     std::string s;
     std::cin >> s;
     std::cout << s << std::endl;
-
-    for(int i = 0; i < s.size(); i++)
-    {
-        if(s[i] == 'a')
-        {
-            s[i] = 'b';
-        }
-        else if(s[i] == 'b')
-        {
-            s[i] = 'a';
-        }
-    }
     return 0;
 }
